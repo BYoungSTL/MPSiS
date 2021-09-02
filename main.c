b775ba712c73426be51b33d5328adb2a47ae9576
BYoungSTL <61958662+BYoungSTL@users.noreply.github.com>
2021-09-02 21:56:06 +0300

    NE GORIT

--- main.c
+++ main.c
@@ -5,11 +5,18 @@
  */
 int main(void) {
     WDTCTL = WDTPW | WDTHOLD;	// Stop watchdog timer
-    P1DIR = 191;
-    P2DIR = 253;
-    P1DS = BIT7;
-    if(P1BIT7 == 0){
-    	P1OUT &= BIT3;
+    P1OUT = 0x00;
+    P1DIR &= ~BIT7;
+    P2DIR &= ~BIT2;
+    P1DIR |= BIT3;
+    P1SEL = 0;
+    P1REN |= BIT3;
+    //P1DS |= BIT7;
+    //__no_operation();
+    while(1){
+    	if(P1IN&BIT7 == 0){
+    		P1OUT |= BIT3;
+    	}
     }
 	return 0;
 }
