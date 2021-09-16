c9479aced010ebda1c1097c45f1e78269188b64b
BYoungSTL <61958662+BYoungSTL@users.noreply.github.com>
2021-09-16 21:09:01 +0300

    no ready stady andrei loh

--- int.c
+++ int.c
@@ -2,21 +2,29 @@
 /*
  * main.c
  */
-void delay(void);
+#pragma vector = PORT1_VECTOR
+__interrupt void S1_interrupt_handler(void)
+{
+	if ((P1IN & BIT7) == 1){
+	    if((P2IN & BIT2) == 0){
+	    	if((P1OUT & BIT3) == 0){
+	    		P1OUT |= BIT3;
+	    	}else{
+	    		P1OUT &= ~BIT3;
+	    	}
+	    }
+	}
+}
 
 void main(void) {
     WDTCTL = WDTPW | WDTHOLD;	// Stop watchdog timer
 
-    P1OUT = 0;
-    P1OUT |= BIT7;
-    //P1DIR &= ~BIT7;
+    P1DIR &= ~BIT7;
     P1REN |= BIT7;
     P1SEL = 0;
 
-    P2OUT = 0;
-    P2OUT |= BIT2;
     P2REN |= BIT2;
-    //P2DIR &= ~BIT2;
+    P2DIR &= ~BIT2;
     P2SEL = 0;
 
     P1DIR |= BIT2;
@@ -26,56 +34,15 @@ void main(void) {
     P1DIR |= BIT3;
     P1REN |= BIT3;
 
-    P1IE |= BIT7;
-    P2IE |= BIT2;
     P1IES &= ~BIT7;
     P2IES &= ~BIT2;
     P1IFG &= ~BIT7;
     P2IFG &= ~BIT2;
-
-    int butt1_flag = 0;
-    int butt2_flag = 0;
+    P1IE |= BIT7;
+    P2IE |= BIT2;
 
     __bis_SR_register(GIE);
     while(1){
-    	int butt1 = (P1IN & BIT7) == 0;
-    	int butt2 = (P2IN & BIT2) == 0;
-
-
-    	//start of first LED
-    	if (butt2 == 1 && butt2_flag == 0) {
-    		butt2_flag = 1;
-    	}
-
-    	if (butt1 == 0 && butt1_flag == 0){
-    		if(butt2_flag == 1 && butt2 == 0){
-    			if((P1OUT & BIT3) == 0){
-    				P1OUT |= BIT3;
-    			}else{
-    				P1OUT &= ~BIT3;
-    			}
-    			butt2_flag = 0;
-    		}
-    	}
-    	//end of first LED
-
-    	//start of second LED
-    	if (butt1 == 1 && butt1_flag == 0) {
-    		butt1_flag = 1;
-    	}
-
-    	if(butt1_flag == 1 && butt1 == 0){
-    		if((P1OUT & BIT2) == 0){
-    			P1OUT |= BIT2;
-    			butt1_flag = 0;
-    		}else{
-    			if(butt2 == 1){
-    				P1OUT &= ~BIT2;
-    				butt1_flag = 0;
-    			}
-    		}
-    	}
-    	//end of second LED
+    	__no_operation();
     }
 }
-
